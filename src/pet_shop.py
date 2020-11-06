a5f2fcc3ccca14b86c254a34c493440e447c55ec
Iain Forbes <i.forbes77@gmail.com>
2020-11-06 12:31:57 +0000

    Added find by name

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -33,6 +33,14 @@ def get_pets_by_breed(pet_shop, breed):
     
     return found_pets
 
+# Find Pet by name 
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+            
+
+
 
 
 
