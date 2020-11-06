8950b834c5155f90a2fa9f94517fbc176fddff6a
Iain Forbes <i.forbes77@gmail.com>
2020-11-06 09:42:44 +0000

    1st commit, add func name" d q

---
+++ src/pet_shop.py
@@ -0,0 +1,9 @@
+# WRITE YOUR FUNCTIONS HERE
+
+# Returns Name of Pet Shop
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+# Returns Total Cash 
+def get_total_cash(pet_shop):
+    return["admin"]
